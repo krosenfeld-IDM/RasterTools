588a42f0c4b6d16ed3df9a5f3abd4e4e87900e84
Kurt Frey <kfrey@idmod.org>
2025-02-13 14:49:03 -0800

    Corrections

--- .github/scripts/bump_version.py
+++ .github/scripts/bump_version.py
@@ -1,10 +1,11 @@
 import os
 
 from datetime import datetime
-from packaging.version import parse
 
 VNS = '__version__'
 VDS = '__versiondate__'
+EQS = ' = \''
+NLS = '\'\n'
 
 
 def bump_version():
@@ -15,19 +16,21 @@ def bump_version():
     with open(fname) as fid01:
         flines = fid01.readlines()
 
-    with open(fname, 'w') as fid01:
+    with open(fname, 'w') as fid02:
         for lval in flines:
             if (lval.startswith(VDS)):
                 dval = datetime.today().strftime('%Y-%m-%d')
-                nline = VDS + ' = ' + dval + '\n'
-                fid01.write(nline)
+                nline = VDS + EQS + dval + NLS
+                fid02.write(nline)
             elif (lval.startswith(VNS)):
-                ver = parse(lval.split('\'')[1])
-                nver = str(ver.major) + '.' + str(ver.minor) + '.'
-                nver = nver + str(ver.micro + 1)
-                nline = VNS + ' = \'' + nver + '\'\n'
+                ver = lval.split('\'')[1]
+                vnum = ver.split('.')
+                nver = vnum[0] + '.' + vnum[1] + '.'
+                nver = nver + str(int(vnum[2]) + 1)
+                nline = VNS + EQS + nver + NLS
+                fid02.write(nline)
             else:
-                fid01.write(lval)
+                fid02.write(lval)
 
     return None
 
